315e13c01e94c23f2b993aad0820c22def1389e1
bluesky00511 <bluesky00511@gmail.com>
2025-01-30 02:51:48 -0500

    #5 implemented email verfication screen

--- src/navigation/AuthNavigator.js
+++ src/navigation/AuthNavigator.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignUpScreen from '../screens/signupScreen';
+import VerificationScreen from '../screens/signupScreen/verification'
 
 const Stack = createNativeStackNavigator();
 
@@ -9,16 +10,17 @@ export const AuthNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
+        {/* <Stack.Screen
           name="SignUp"
           component={SignUpScreen}
           options={{ headerShown: false }}
-        />
-        {/* <Stack.Screen
-          name="onboarding"
-          component={OnBoardingScreen}
-          options={{ headerShown: false }}
         /> */}
+
+        <Stack.Screen
+          name="Verification"
+          component={VerificationScreen}
+          options={{ headerShown: false }}
+        />
         {/* You can add more screens here */}
       </Stack.Navigator>
     </NavigationContainer>
